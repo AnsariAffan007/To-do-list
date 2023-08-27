533ecfff35bbb549c8555d8d9f523ff780a6dccb
Affan Ansari <ansariaffan014@gmail.com>
2023-08-27 21:06:22 +0530

    update google oauth callback url

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ passport.deserializeUser(function (id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/To-Do-List",
+    callbackURL: "https://todolist-h6mr.onrender.com/auth/google/To-Do-List",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 },
     function (accessToken, refreshToken, profile, cb) {
